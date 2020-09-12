1a7855eb7b1bc2e6df975a18899956ab08fb9348
Jared Flores <jared_flores@cs.ucsb.edu>
2020-09-11 18:29:46 -0700

    Entity Skeleton

---
+++ docs/js/game.js
@@ -0,0 +1,26 @@
+function EntityState() {
+	this.x = 0;
+	this.y = 0;
+	this.dx = 0;
+	this.dy = 0;
+}
+
+function SerpentHead() {
+	this.state = new EntityState();
+}
+
+function SerpentBody() {
+	this.state = new EntityState();
+}
+
+function SerpentTail() {
+	this.state = new EntityState();
+}
+
+function Bullet() {
+	this.state = new EntityState();
+}
+
+function Player() {
+	this.state = new EntityState();
+}
