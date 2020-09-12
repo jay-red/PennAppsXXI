05f23d26675af3479a2a75121765ffb15e4f0651
Jared Flores <jared_flores@cs.ucsb.edu>
2020-09-12 05:04:56 -0700

    he moves

--- docs/js/game.js
+++ docs/js/game.js
@@ -3,8 +3,16 @@ var SCALE = 3;
 var SERPENT_ACCELERATION = 0.0003 * SCALE,
 	SERPENT_VELOCITY = 0.67 * SCALE;
 
-var TERMINAL_VELOCITY = 0.5,
-	GRAVITY = 0.0013;
+var TERMINAL_VELOCITY = 0.5 * SCALE,
+	GRAVITY = 0.0013 * SCALE;
+
+var TERMINAL_RUNNING = 0.175 * SCALE,
+	RUNNING = 0.0007 * SCALE,
+	FRICTION = 0.0004 * SCALE;
+
+var KEY_A = 65,
+	KEY_D = 68,
+	KEY_SPACE = 32;
 
 var POWER_ICE = 0,
 	POWER_EARTH = 1,
@@ -18,7 +26,7 @@ var WIDTH_TILE = 8 * SCALE,
 	HEIGHT_TILE = 8 * SCALE;
 
 var WIDTH_MAP = TWIDTH_MAP * WIDTH_TILE,
-	HEIGHT_MAP = TWIDTH_MAP * WIDTH_TILE;
+	HEIGHT_MAP = THEIGHT_MAP * WIDTH_TILE;
 
 var WIDTH_VIEW = 80 * WIDTH_TILE,
 	HEIGHT_VIEW = WIDTH_VIEW * window.screen.height / window.screen.width,
@@ -48,7 +56,7 @@ var WIDTH_TAIL = 21 * SCALE,
 	HHEIGHT_TAIL = 6 * SCALE;
 
 var WIDTH_PLAYER = 7 * SCALE,
-	HEIGHT_PLAYER = 17 * SCALE
+	HEIGHT_PLAYER = 17 * SCALE,
 	HWIDTH_PLAYER = WIDTH_PLAYER / 2,
 	HHEIGHT_PLAYER = HEIGHT_PLAYER / 2;
 
@@ -148,15 +156,24 @@ function Player( idx ) {
 	this.anim = this.anim_idle;
 	this.last_anim = null;
 	this.left = true;
+	this.running = 0;
 }
 
 function Me( idx ) {
 	this.idx = idx;
+	this.player = players[ idx ];
+	this.keys = {};
+	this.last_update = -1;
+}
+
+function key_down( key ) {
+	return me.keys.hasOwnProperty( key ) && me.keys[ key ];
 }
 
 var players = [];
 var segments = [];
 var bullets = [ [], [], [], [] ];
+var me = null;
 
 function activate_player() {
 	var player,
@@ -177,7 +194,7 @@ function activate_player() {
 	}
 	player.state.active = true;
 	player.alive = true;
-	return -1;
+	return idx;
 }
 
 function activate_bullet( pid ) {
@@ -247,6 +264,20 @@ function create_serpent( split, power, ai, length ) {
 	return serpent;
 }
 
+function update_me( ts ) {
+	if( me == null ) return;
+	var DOWN_A = key_down( KEY_A ),
+		DOWN_D = key_down( KEY_D ),
+		DOWN_SPACE = key_down( KEY_SPACE );
+	if( DOWN_A && !DOWN_D ) {
+		me.player.running = -1;
+	} else if( DOWN_D && !DOWN_A ) {
+		me.player.running = 1;
+	} else {
+		me.player.running = 0;
+	}
+}
+
 function update_player( ts, player ) {
 	if( player.state.last_update == -1 ) player.state.last_update = ts;
 	var ticks = ts - player.state.last_update;
@@ -254,11 +285,36 @@ function update_player( ts, player ) {
 	var x_last = player.state.x,
 		y_last = player.state.y;
 
-	var dx = 0, 
-		dy = 0;
+	var dx = player.state.dx, 
+		dy = player.state.dy;
 
-	var x_next = player.state.x + dx,
-		y_next = player.state.y + dy;
+	if( Math.abs( dx ) < TERMINAL_RUNNING ) {
+		dx += player.running * RUNNING * ticks;
+	}
+	
+	
+	if( player.running == 0 && Math.abs( dx ) < 0.05 ) dx = 0;
+	else if( dx < 0 ) dx += FRICTION * ticks;
+	else dx -= FRICTION * ticks;
+
+	dy += GRAVITY * ticks;
+	if( dy > TERMINAL_VELOCITY ) {
+		dy = TERMINAL_VELOCITY;
+	}
+
+	var x_next = player.state.x + dx * ticks,
+		y_next = player.state.y + dy * ticks;
+
+	if( x_next < 0 ) x_next = 0;
+	if( y_next < 0 ) y_next = 0;
+
+	if( x_next + WIDTH_PLAYER >= WIDTH_MAP ) x_next = WIDTH_MAP - WIDTH_PLAYER;
+	if( y_next + HEIGHT_PLAYER >= HEIGHT_MAP ) y_next = HEIGHT_MAP - HEIGHT_PLAYER;
+
+	player.state.dx = dx;
+	player.state.dy = dy;
+	player.state.x = x_next;
+	player.state.y = y_next;
 
 	player.state.last_update = ts;
 }
@@ -360,7 +416,7 @@ function update_players( ts ) {
 	var player;
 	for( var i = 0; i < players.length; ++i ) {
 		player = players[ i ];
-		if( player.active ) {
+		if( player.state.active ) {
 			update_player( ts, player );
 		}
 	}
@@ -398,6 +454,8 @@ function draw_players( ts ) {
 	for( var i = 0; i < players.length; ++i ) {
 		player = players[ i ];
 		if( ( !player.state.active ) || ( !player.alive ) ) continue;
+		if( player.running ) player.anim = player.anim_run;
+		else player.anim = player.anim_idle;
 		if( player.last_anim != player.anim ) {
 			player.anim.frame = 0;
 			player.last_anim = player.anim;
@@ -415,7 +473,7 @@ function draw_players( ts ) {
 			img = player.anim.sprites[ 1 ].img;
 			offset_x = player.anim.width - player.anim.offset_x;
 		}
-		ctx_ent.drawImage( img, player.anim.width * player.anim.frame, 0, player.anim.width, player.anim.height, player.state.x - offset_x, player.state.y - offset_y, player.anim.width, player.anim.height );
+		ctx_ent.drawImage( img, player.anim.width * player.anim.frame, 0, player.anim.width, player.anim.height, Math.round( player.state.x - offset_x ), Math.round( player.state.y - offset_y ), player.anim.width, player.anim.height );
 	}
 }
 
@@ -462,6 +520,8 @@ function draw_segments() {
 }
 
 function draw_viewport() {
+	x_view = Math.round( me.player.state.x + HWIDTH_PLAYER - HWIDTH_VIEW );
+	y_view = Math.round( me.player.state.y + HHEIGHT_PLAYER - HHEIGHT_VIEW );
 	ctx_view.clearRect( 0, 0, WIDTH_VIEW, HEIGHT_VIEW );
 	ctx_view.drawImage( canvas_tile, -x_view, -y_view );
 	ctx_view.drawImage( canvas_ent, -x_view, -y_view );
@@ -469,10 +529,10 @@ function draw_viewport() {
 
 function game_loop( ts ) {
 	ctx_ent.clearRect( 0, 0, WIDTH_MAP, HEIGHT_MAP );
+	update_me( ts );
 	update_segments( ts );
 	update_players( ts );
 	update_bullet( ts );
-	//draw_bullets();
 	draw_players( ts );
 	draw_segments();
 	draw_viewport();
@@ -492,8 +552,10 @@ function get_tile( x, y ) {
 function draw_tile( x, y ) {
 	var tx = x * WIDTH_TILE,
 		ty = y * HEIGHT_TILE;
-	if( get_tile( x, y ) > 1 ) {
+	if( get_tile( x, y ) > 1 || true ) {
 		ctx_tile.clearRect( tx, ty, WIDTH_TILE, HEIGHT_TILE );
+		//ctx_tile.fillStyle="#FFFFFF";
+		ctx_tile.fillRect( tx, ty, WIDTH_TILE, HEIGHT_TILE );
 	}
 }
 
@@ -503,10 +565,23 @@ function handle_mousemove( evt ) {
 	y_mouse = evt.clientY - rect.top;
 }
 
+function handle_keydown( evt ) {
+	me.keys[ evt.keyCode ] = true;
+}
+
+function handle_keyup( evt ) {
+	me.keys[ evt.keyCode ] = false;
+}
+
 function init_listeners() {
+	//ctx_tile.fillRect( 0, 0, WIDTH_MAP, HEIGHT_MAP );
 	canvas_view.addEventListener( "mousemove", handle_mousemove );
+	me = new Me( activate_player() );
+	if( me != null ) {
+		document.addEventListener( "keydown", handle_keydown );
+		document.addEventListener( "keyup", handle_keyup );
+	}
 	init_segments( [ create_serpent( false, 0, 1, 20 ) ] );
-	activate_player();
 	window.requestAnimationFrame( game_loop );
 }
 
